5c7feeba81123149def039d7b7a067677894e116
Irakli <Iraklyoda42@gmail.com>
2025-04-10 21:47:40 +0400

    fix: fix main activity layout

--- app/src/main/java/com/tbacademy/nextstep/presentation/MainActivity.kt
+++ app/src/main/java/com/tbacademy/nextstep/presentation/MainActivity.kt
@@ -3,15 +3,22 @@ package com.tbacademy.nextstep.presentation
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.credentials.GetCredentialRequest
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption
 import com.tbacademy.nextstep.R
+import com.tbacademy.nextstep.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
